65c9053d8763f5e531a781102d4de2ddce02b253
Le Quang Lam <work.lamsun@gmail.com>
2024-08-08 18:09:28 +0700

    calendar fragment interface

--- app/src/main/java/com/example/todolist/fragment/CalendarFragment.java
+++ app/src/main/java/com/example/todolist/fragment/CalendarFragment.java
@@ -3,10 +3,13 @@ package com.example.todolist.fragment;
 import static com.kizitonwose.calendar.core.ExtensionsKt.daysOfWeek;
 import static com.kizitonwose.calendar.core.ExtensionsKt.firstDayOfWeekFromLocale;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -29,26 +32,35 @@ import java.time.format.TextStyle;
 import java.util.List;
 import java.util.Locale;
 
+import kotlin.Unit;
+
 public class CalendarFragment extends Fragment {
     private FragmentCalendarBinding binding;
     private CalendarView calendarView;
+    private LocalDate selectedDate = null;
+    LocalDate mCurrentMonth = YearMonth.now().atDay(1);
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         calendarView = binding.calendarView;
+        setupCalendar();
+        setWidgets();
+        setEvents();
         return binding.getRoot();
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        setWidgets();
+    private void setupCalendar() {
+        YearMonth currentMonth = YearMonth.now();
+        YearMonth startMonth = currentMonth.minusMonths(10);
+        YearMonth endMonth = currentMonth.plusMonths(10);
+        DayOfWeek firstDayOfWeek = firstDayOfWeekFromLocale();
+        calendarView.setup(startMonth, endMonth, firstDayOfWeek);
+        calendarView.scrollToMonth(currentMonth);
     }
 
     private void setWidgets() {
         calendarView.setDayBinder(new MonthDayBinder<DayViewContainer>() {
-            @NonNull
             @Override
             public DayViewContainer create(@NonNull View view) {
                 return new DayViewContainer(view);
@@ -57,26 +69,40 @@ public class CalendarFragment extends Fragment {
             @Override
             public void bind(@NonNull DayViewContainer container, CalendarDay calendarDay) {
                 container.calendarDayText.setText(String.valueOf(calendarDay.getDate().getDayOfMonth()));
-                if (calendarDay.getPosition() == DayPosition.MonthDate){
-                    container.calendarDayText.setTextColor(getResources().getColor(R.color.black));
-                } else{
-                    container.calendarDayText.setVisibility(View.GONE);
+
+                // Xử lý hiển thị ngày trong tháng
+                if (calendarDay.getPosition() == DayPosition.MonthDate) {
+                    container.calendarDayText.setTextColor(getResources().getColor(R.color.black, getResources().newTheme()));
+                } else {
+                    container.calendarDayText.setTextColor(getResources().getColor(R.color.grey_text, getResources().newTheme()));
+                }
+
+                container.day = calendarDay;
+                TextView textView = container.calendarDayText;
+                textView.setText(String.valueOf(calendarDay.getDate().getDayOfMonth()));
+
+                if (calendarDay.getPosition() == DayPosition.MonthDate) {
+                    textView.setVisibility(View.VISIBLE);
+                    if (calendarDay.getDate().equals(selectedDate)) {
+                        // Nếu là ngày được chọn, thay đổi màu chữ và nền.
+                        textView.setTextColor(Color.WHITE);
+                        textView.setBackgroundResource(R.drawable.background_selection_day);
+                    } else {
+                        // Nếu không phải ngày được chọn, đặt lại màu chữ và nền.
+                        textView.setTextColor(Color.BLACK);
+                        textView.setBackground(null);
+                    }
+                } else {
+                    textView.setVisibility(View.VISIBLE);
+                    Typeface typeface = ResourcesCompat.getFont(requireContext(), R.font.inter_regular);
+                    textView.setTypeface(typeface);
                 }
             }
         });
 
-        LocalDate currentDate = LocalDate.now();
-        YearMonth currentMonth = YearMonth.from(currentDate);
-        YearMonth startMonth = currentMonth.minusMonths(100);
-        YearMonth endMonth = currentMonth.plusMonths(100);
-        DayOfWeek firstDayOfWeek = firstDayOfWeekFromLocale();
-        List<DayOfWeek> daysOfWeek = daysOfWeek(firstDayOfWeek = DayOfWeek.SUNDAY);
-        calendarView.setup(startMonth, endMonth, daysOfWeek.get(0));
-        calendarView.scrollToMonth(currentMonth);
-
         ViewGroup titlesContainer = binding.titlesContainer.getRoot();
-        int childCount = titlesContainer.getChildCount();
-        for (int index = 0; index < childCount; index++) {
+        List<DayOfWeek> daysOfWeek = daysOfWeek(firstDayOfWeekFromLocale());
+        for (int index = 0; index < titlesContainer.getChildCount(); index++) {
             TextView textView = (TextView) titlesContainer.getChildAt(index);
             DayOfWeek dayOfWeek = daysOfWeek.get(index);
             String title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault());
@@ -84,24 +110,57 @@ public class CalendarFragment extends Fragment {
         }
     }
 
+    private void setEvents() {
+        binding.buttonPreMonth.setOnClickListener(v -> {
+            mCurrentMonth = mCurrentMonth.minusMonths(1);
+            updateCalendar(mCurrentMonth);
+        });
+
+        binding.buttonNextMonth.setOnClickListener(v -> {
+            mCurrentMonth = mCurrentMonth.plusMonths(1);
+            updateCalendar(mCurrentMonth);
+        });
+
+        binding.calendarView.setMonthScrollListener(calendarMonth -> {
+            mCurrentMonth = calendarMonth.getYearMonth().atDay(1);
+            updateMonthYearDisplay();
+            return Unit.INSTANCE;
+        });
+    }
+
+    private void updateCalendar(LocalDate currentMonth) {
+        YearMonth yearMonth = YearMonth.from(currentMonth);
+        calendarView.setup(yearMonth.minusMonths(100), yearMonth.plusMonths(100), firstDayOfWeekFromLocale());
+        calendarView.scrollToMonth(yearMonth);
+        updateMonthYearDisplay();
+    }
+
+    private void updateMonthYearDisplay() {
+        YearMonth yearMonth = YearMonth.from(mCurrentMonth);
+        binding.textViewMonth.setText(mCurrentMonth.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()));
+        binding.textViewYear.setText(String.valueOf(yearMonth.getYear()));
+    }
+
     public class DayViewContainer extends ViewContainer {
-        private LocalDate selectedDate;
         private CalendarDay day;
         public final TextView calendarDayText;
 
-        public DayViewContainer(View view) {
+        public DayViewContainer(@NonNull View view) {
             super(view);
             calendarDayText = view.findViewById(R.id.calendarDayText);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (day.getPosition() == DayPosition.MonthDate) {
-                        LocalDate currentSelection = selectedDate;
-                        if (currentSelection == day.getDate()) {
-                            selectedDate = null;
-                            calendarView.notifyDateChanged(currentSelection);
+
+            view.setOnClickListener(v -> {
+                if (day.getPosition() == DayPosition.MonthDate) {
+                    LocalDate currentSelection = selectedDate;
+                    if (currentSelection == day.getDate()) {
+                        selectedDate = null; // Bỏ chọn ngày nếu ngày đã được chọn trước đó.
+                        calendarView.notifyDateChanged(currentSelection); // Cập nhật lại giao diện cho ngày bị bỏ chọn.
+                    } else {
+                        selectedDate = day.getDate(); // Chọn ngày mới.
+                        calendarView.notifyDateChanged(day.getDate()); // Cập nhật lại giao diện cho ngày được chọn.
+                        if (currentSelection != null) {
+                            calendarView.notifyDateChanged(currentSelection); // Bỏ chọn ngày trước đó nếu có.
                         }
-                        selectedDate = currentSelection;
                     }
                 }
             });
